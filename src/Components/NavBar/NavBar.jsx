d4a08aba20edf46b27035d89dd33a5e364d95f0e
Smael Nicolas <nico.smael@gmail.com>
2022-05-06 19:04:51 -0300

    tabs navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+
+import "./navBar.css";
+import { Home } from "../../Pages/Home/Home";
 
 export const NavBar = () => {
-	return <div>NavBar</div>;
+	const [value, setValue] = useState(0);
+
+	function TabPanel(props) {
+		const { children, value, index, ...other } = props;
+
+		return (
+			<div
+				role='tabpanel'
+				hidden={value !== index}
+				id={`simple-tabpanel-${index}`}
+				aria-labelledby={`simple-tab-${index}`}
+				{...other}
+			>
+				{value === index && (
+					<Box sx={{ p: 3 }}>
+						<Typography>{children}</Typography>
+					</Box>
+				)}
+			</div>
+		);
+	}
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	return (
+		// <Box>
+		// 	<BottomNavigation
+		// 		showLabels
+		// 		value={value}
+		// 		onChange={(event, newValue) => {
+		// 			setValue(newValue);
+		// 		}}
+		// 	>
+		// 		<BottomNavigationAction label='Inicio' />
+		// 		<BottomNavigationAction label='Jeans' />
+		// 		<BottomNavigationAction label='Remeras' />
+		// 		<BottomNavigationAction label='Sweaters' />
+		// 		<BottomNavigationAction label='Accesorios' />
+		// 		<BottomNavigationAction label='Contacto' />
+		// 	</BottomNavigation>
+		// </Box>
+		<>
+			<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+				<Tabs
+					centered={true}
+					value={value}
+					onChange={handleChange}
+					aria-label='basic tabs example'
+				>
+					<Tab label='Inicio' />
+					<Tab label='Jeans' />
+					<Tab label='Remeras' />
+					<Tab label='Sweaters' />
+					<Tab label='Accesorios' />
+					<Tab label='Contacto' />
+				</Tabs>
+			</Box>
+			<TabPanel value={value} index={0}>
+				<Home />
+			</TabPanel>
+			<TabPanel value={value} index={1}>
+				Item Two
+			</TabPanel>
+			<TabPanel value={value} index={2}>
+				Item Three
+			</TabPanel>
+			<TabPanel value={value} index={3}>
+				Item 4
+			</TabPanel>
+			<TabPanel value={value} index={4}>
+				Item 5
+			</TabPanel>
+			<TabPanel value={value} index={5}>
+				Item 6
+			</TabPanel>
+		</>
+	);
 };
