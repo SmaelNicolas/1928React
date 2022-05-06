fcc76e8c4b99a62dcb8b04b63a0c8c5fbafb92d0
Smael Nicolas <nico.smael@gmail.com>
2022-05-06 19:05:13 -0300

    imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { NavBar } from "./Components/NavBar/NavBar";
+import { ToolBar } from "./Components/ToolBar/ToolBar";
 import { TopBar } from "./Components/TopBar/TopBar";
 
 function App() {
 	return (
 		<div className='app'>
 			<TopBar />
+			<ToolBar />
+			<NavBar />
 		</div>
 	);
 }
