95b21cca1128d954cb852abd405646fe7dec1d65
Smael Nicolas <nico.smael@gmail.com>
2022-05-06 19:04:45 -0300

    item showcase

---
+++ src/Components/ItemShowcase/ItemShowcase.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ItemCard } from "../ItemCard/ItemCard";
+import "./itemShowcase.css";
+
+export const ItemShowcase = () => {
+	return (
+		<div className='itemShowcaseContainer'>
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+			<ItemCard />
+		</div>
+	);
+};
