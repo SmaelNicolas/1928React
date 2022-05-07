a01ac7cdaa6d020a7bc9a255985d75c4de48eb6b
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 01:41:57 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./Components/Footer/Footer";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { ToolBar } from "./Components/ToolBar/ToolBar";
 import { TopBar } from "./Components/TopBar/TopBar";
@@ -9,6 +10,7 @@ function App() {
 			<TopBar />
 			<ToolBar />
 			<NavBar />
+			<Footer />
 		</div>
 	);
 }
