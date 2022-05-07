e0400807555d2fea04eae5ed2562d32f1fa60bb1
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 01:42:04 -0300

    button mroe

---
+++ src/Components/ButtonMore/ButtonMore.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import React from "react";
+import "./buttonMore.css";
+
+export const ButtonMore = () => {
+	return (
+		<Button variant='contained' size='large' className='buttonMore'>
+			VER TODOS LOS PRODUCTOS
+		</Button>
+	);
+};
