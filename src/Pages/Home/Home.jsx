9b20dc6bf13d3d13957f80125d580cd4420a0dd9
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 01:42:33 -0300

    home display

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ButtonMore } from "../../Components/ButtonMore/ButtonMore";
+import { Divider } from "../../Components/Divider/Divider";
 import { ItemShowcase } from "../../Components/ItemShowcase/ItemShowcase";
 import { SwiperHome } from "../../Components/SwiperHome/SwiperHome";
 import "./home.css";
@@ -7,7 +9,14 @@ export const Home = () => {
 	return (
 		<div className='homeContainer'>
 			<SwiperHome />
+			<Divider />
+			<Divider />
 			<ItemShowcase />
+			<Divider />
+			<Divider />
+			<ButtonMore />
+			<Divider />
+			<Divider />
 		</div>
 	);
 };
