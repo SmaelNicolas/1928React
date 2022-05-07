7cd52aeefa26887659a3aa95b8a498a9c7c81270
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 14:08:11 -0300

    fix styles

--- src/Components/ContactDirectInfo/ContactDirectInfo.jsx
+++ src/Components/ContactDirectInfo/ContactDirectInfo.jsx
@@ -13,11 +13,12 @@ import "./contactDirectInfo.css";
 
 export const ContactDirectInfo = () => {
 	return (
-		<Stack direction='row' spacing={1}>
+		<Stack direction='row' spacing={1} className='stackContact'>
 			<Chip
 				icon={<WhatsAppIcon />}
 				label='2914182420'
 				variant='outlined'
+				className='stackContactChip'
 				onClick={() =>
 					openInNewTab(
 						"https://api.whatsapp.com/send/?phone=542914182420"
@@ -28,6 +29,7 @@ export const ContactDirectInfo = () => {
 				icon={<InstagramIcon />}
 				label='@1928showroom'
 				variant='outlined'
+				className='stackContactChip'
 				onClick={() =>
 					openInNewTab("https://www.instagram.com/1928showroom/")
 				}
@@ -36,6 +38,7 @@ export const ContactDirectInfo = () => {
 				icon={<FacebookIcon />}
 				label='/1928showroom'
 				variant='outlined'
+				className='stackContactChip'
 				onClick={() =>
 					openInNewTab("https://www.facebook.com/1928showroom/")
 				}
@@ -44,6 +47,7 @@ export const ContactDirectInfo = () => {
 				icon={<PlaceIcon />}
 				label='Panama 1259 , Bahía Blanca'
 				variant='outlined'
+				className='stackContactChip'
 				onClick={() =>
 					openInNewTab(
 						"https://www.google.com.ar/maps/place/1928Showroom/@-38.7014743,-62.273796,15z/data=!4m5!3m4!1s0x0:0x1faf91a144097c96!8m2!3d-38.7014743!4d-62.273796"
