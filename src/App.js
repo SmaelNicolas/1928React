e9049ee33d458ae4342dcbe2b4b8f5905012ef03
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 19:58:37 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
 import { Footer } from "./Components/Footer/Footer";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { ToolBar } from "./Components/ToolBar/ToolBar";
 import { TopBar } from "./Components/TopBar/TopBar";
 import { CartProvider } from "./Context/CartContext";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
 	return (
@@ -11,7 +12,10 @@ function App() {
 			<div className='app'>
 				<TopBar />
 				<ToolBar />
-				<NavBar />
+				<Routes>
+					<Route exact path='/' element={<NavBar />} />
+					{/* <Route path='order' element={<About />} /> */}
+				</Routes>
 				<Footer />
 			</div>
 		</CartProvider>
