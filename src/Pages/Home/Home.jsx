8ddaf408f25bfdc2071b9fbaa6f45c91d67b1519
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 19:27:37 -0300

    pass data to showcase

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonMore } from "../../Components/ButtonMore/ButtonMore";
 import { Divider } from "../../Components/Divider/Divider";
 import { ItemShowcase } from "../../Components/ItemShowcase/ItemShowcase";
 import { SwiperHome } from "../../Components/SwiperHome/SwiperHome";
+import dataDummy from "../../Data/dummyData";
 import "./home.css";
 
 export const Home = () => {
+	const [data] = useState(dataDummy);
+
 	return (
 		<div className='homeContainer'>
 			<SwiperHome />
 			<Divider />
 			<Divider />
-			<ItemShowcase />
+			<ItemShowcase data={data} />
 			<Divider />
 			<Divider />
 			<ButtonMore />
