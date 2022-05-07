d603dada259d8e0d9ee5b0c9d777083c6e85eb6d
Smael Nicolas <nico.smael@gmail.com>
2022-05-07 19:25:08 -0300

    cart provider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Footer } from "./Components/Footer/Footer";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { ToolBar } from "./Components/ToolBar/ToolBar";
 import { TopBar } from "./Components/TopBar/TopBar";
+import { CartProvider } from "./Context/CartContext";
 
 function App() {
 	return (
-		<div className='app'>
-			<TopBar />
-			<ToolBar />
-			<NavBar />
-			<Footer />
-		</div>
+		<CartProvider>
+			<div className='app'>
+				<TopBar />
+				<ToolBar />
+				<NavBar />
+				<Footer />
+			</div>
+		</CartProvider>
 	);
 }
 
