9ad1acfc3fed4c3149f97af07dce67e950fccf5e
Smael Nicolas <nico.smael@gmail.com>
2022-05-08 14:20:03 -0300

    responsive bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,7 +5,6 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 
-import "./navBar.css";
 import { Home } from "../../Pages/Home/Home";
 import { Jeans } from "../../Pages/Jeans/Jeans";
 import { Remeras } from "../../Pages/Remeras/Remeras";
@@ -13,8 +12,17 @@ import { Sweaters } from "../../Pages/Sweaters/Sweaters";
 import { Accesorios } from "../../Pages/Accesorios/Accesorios";
 import { Contact } from "../../Pages/Contact/Contact";
 
+import { styled } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import MenuIcon from "@mui/icons-material/Menu";
+import Fab from "@mui/material/Fab";
+
+import "./navBar.css";
+
 export const NavBar = () => {
 	const [value, setValue] = useState(0);
+	const [showMenu, setShowMenu] = useState(false);
 
 	function TabPanel(props) {
 		const { children, value, index, ...other } = props;
@@ -38,15 +46,41 @@ export const NavBar = () => {
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
+	const StyledFab = styled(Fab)({
+		position: "absolute",
+		zIndex: 1,
+		top: -30,
+		left: 0,
+		right: 0,
+		margin: "0 auto",
+	});
+
+	const handleClickMenu = () => {
+		setShowMenu(!showMenu);
+	};
 
 	return (
 		<>
-			<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+			<AppBar className='appBarResponsive'>
+				<Toolbar>
+					<StyledFab
+						aria-label='menu'
+						onClick={() => handleClickMenu()}
+					>
+						<MenuIcon />
+					</StyledFab>
+				</Toolbar>
+			</AppBar>
+			<Box
+				sx={{ borderBottom: 1, borderColor: "divider" }}
+				className='tabsBar'
+			>
 				<Tabs
 					centered={true}
 					value={value}
 					onChange={handleChange}
 					aria-label='basic tabs example'
+					className='tabsContainer'
 				>
 					<Tab label='Inicio' />
 					<Tab label='Jeans' />
@@ -55,6 +89,34 @@ export const NavBar = () => {
 					<Tab label='Accesorios' />
 					<Tab label='Contacto' />
 				</Tabs>
+				{showMenu && (
+					<Tabs
+						centered={true}
+						value={value}
+						onChange={handleChange}
+						aria-label='basic tabs example'
+						className='tabsListComponent'
+					>
+						<Tab label='Inicio' onClick={() => handleClickMenu()} />
+						<Tab label='Jeans' onClick={() => handleClickMenu()} />
+						<Tab
+							label='Remeras'
+							onClick={() => handleClickMenu()}
+						/>
+						<Tab
+							label='Sweaters'
+							onClick={() => handleClickMenu()}
+						/>
+						<Tab
+							label='Accesorios'
+							onClick={() => handleClickMenu()}
+						/>
+						<Tab
+							label='Contacto'
+							onClick={() => handleClickMenu()}
+						/>
+					</Tabs>
+				)}
 			</Box>
 			<TabPanel value={value} index={0}>
 				<Home />
