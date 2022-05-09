07391e70411ad01584614c8dfb72e8b681092b05
Smael Nicolas <nico.smael@gmail.com>
2022-05-09 20:29:39 -0300

    add map to show items

--- src/Components/ItemShowcase/ItemShowcase.jsx
+++ src/Components/ItemShowcase/ItemShowcase.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ItemCard } from "../ItemCard/ItemCard";
+import getProducts from "../../Firebase/getDataProducts";
+import { Loader } from "../Loader/Loader";
 import "./itemShowcase.css";
 
-export const ItemShowcase = ({ data }) => {
-	return (
+export const ItemShowcase = ({ value }) => {
+	const [loader, setLoader] = useState(true);
+	const [products, setProducts] = useState();
+
+	useEffect(() => {
+		getProducts(setProducts, setLoader);
+	}, []);
+
+	return loader ? (
+		<Loader />
+	) : (
 		<div className='itemShowcaseContainer'>
-			{data.map((item) => (
-				<ItemCard item={item} key={item.id} />
-			))}
+			{products.map((item) => {
+				if (value === "") {
+					return <ItemCard item={item} key={item} />;
+				} else {
+					if (value === "destacado") {
+						return item.outstanding === true ? (
+							<ItemCard item={item} key={item} />
+						) : undefined;
+					} else {
+						return item.type === value ? (
+							<ItemCard item={item} key={item} />
+						) : undefined;
+					}
+				}
+			})}
 		</div>
 	);
 };
