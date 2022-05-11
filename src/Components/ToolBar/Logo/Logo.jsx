d848394317180bd13aaef0f523c51f7753647a3e
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 16:01:26 -0300

    details

--- src/Components/ToolBar/Logo/Logo.jsx
+++ src/Components/ToolBar/Logo/Logo.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoMini from "../../../Assets/logoMini.png";
+import logoEntero from "../../../Assets/logoEntero.png";
 
 import "./logo.css";
 
 export const Logo = () => {
 	return (
 		<Link className='logoName' to='/'>
-			<p className='logoText'>Showroom</p>
 			<img
-				className='logoImg'
+				className='logoImgResponsive'
 				src={logoMini}
 				alt='logo 1928'
 				title='1928 Showroom'
 				loading='lazy'
 			/>
-			<p className='logoText'>Plus Size</p>
+			<img
+				className='logoImg'
+				src={logoEntero}
+				alt='logo 1928'
+				title='1928 Showroom'
+				loading='lazy'
+			/>
 		</Link>
 	);
 };
