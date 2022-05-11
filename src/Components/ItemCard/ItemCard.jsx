cacd5dd812c22630af23d5aeef11da3919355f42
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 15:23:00 -0300

    add modals

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -4,11 +4,20 @@ import Button from "@mui/material/Button";
 import { Divider } from "../Divider/Divider";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { CartContext } from "../../Context/CartContext";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
 
 import "./itemCard.css";
 
 export const ItemCard = ({ item, hide }) => {
 	const { addItemToCart } = useContext(CartContext);
+
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	return (
 		<div className='card'>
@@ -18,8 +27,9 @@ export const ItemCard = ({ item, hide }) => {
 						className='cardImg'
 						src={item.img}
 						alt='img producto'
-						title='producto'
+						title={item.title}
 						loading='lazy'
+						onClick={handleOpen}
 					/>
 					<div className='cardTitle'>{item.title}</div>
 					<div className='cardPrice'>$ {item.price} </div>
@@ -35,6 +45,36 @@ export const ItemCard = ({ item, hide }) => {
 						</Button>
 					)}
 					<Divider />
+				</div>
+				<div>
+					<Modal
+						aria-labelledby='transition-modal-title'
+						aria-describedby='transition-modal-description'
+						open={open}
+						onClose={handleClose}
+						closeAfterTransition
+						BackdropComponent={Backdrop}
+						BackdropProps={{
+							timeout: 500,
+						}}
+					>
+						<Fade in={open}>
+							<Box className='boxModal'>
+								<div
+									id='transition-modal-description'
+									sx={{ mt: 2 }}
+								>
+									<img
+										className='cardImgOnModal'
+										src={item.img}
+										alt='img producto'
+										title={item.title}
+										loading='lazy'
+									/>
+								</div>
+							</Box>
+						</Fade>
+					</Modal>
 				</div>
 			</Paper>
 		</div>
