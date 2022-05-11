2aec6b60c071baac7051f587ebaf38a30a4451b0
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 12:49:34 -0300

    delete duplicated products and generate one array with no undf

--- src/Firebase/getDataSearched.js
+++ src/Firebase/getDataSearched.js
@@ -2,7 +2,6 @@ import { collection, getDocs } from "firebase/firestore";
 import database from "./Firebase";
 
 async function getProductsSearched(setProducts, setLoader, query, setExist) {
-	const newArray = query.split(" ");
 	setLoader(true);
 	const productsCollection = collection(database, "productos");
 	const productDocs = await getDocs(productsCollection);
@@ -10,16 +9,26 @@ async function getProductsSearched(setProducts, setLoader, query, setExist) {
 		id: prod.id,
 		...prod.data(),
 	}));
-	const productsSearched = newArray.map((word) => {
+
+	const queryWordsArray = query.split(" ");
+	//busca y devuelve un array por cada palabra
+	const productsSearched = queryWordsArray.map((word) => {
 		return products.map((prod) => {
 			return prod.description.includes(word) ? prod : undefined;
 		});
 	});
-	setProducts(productsSearched);
+	let arrayComplete = [];
+	//recorre todos los arreglos de todas las palabras y guarda los que son distintos de undefined en un nuevo arreglo
+	productsSearched.map((array) =>
+		array.map((prod) => prod !== undefined && arrayComplete.push(prod))
+	);
+	//elimina todos los duplicados del nuevo arreglo para devolver valores unicos y existentes
+	let uniqueProductsArray = [...new Set(arrayComplete)];
 
-	productsSearched.every((item) => item === undefined)
-		? setExist(false)
-		: setExist(true);
+	setProducts(uniqueProductsArray);
+
+	//si hay un elemento existe un resultado para renderizar
+	uniqueProductsArray.length > 0 ? setExist(true) : setExist(false);
 
 	setTimeout(() => {
 		setLoader(false);
