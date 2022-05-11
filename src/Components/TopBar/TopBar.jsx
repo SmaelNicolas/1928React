b904756aea084061e965cb39960d1ba74c9ec044
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 19:23:39 -0300

    fixes styles

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -3,5 +3,10 @@ import React from "react";
 import "./topbar.css";
 
 export const TopBar = () => {
-	return <div className='topBar'></div>;
+	return (
+		<div className='topBar'>
+			<p>291 418-2420</p>
+			<p>Panamá 1259, Bahía Blanca.</p>
+		</div>
+	);
 };
