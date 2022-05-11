e725a550e0e99ae488274b2344483d607f4d4d30
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 14:49:40 -0300

    helmet provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
 import App from "./App";
 import "./index.css";
 
@@ -8,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
