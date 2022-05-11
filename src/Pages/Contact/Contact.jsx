6de809b61bc6c5da2d980cf71c84fe2156f75efb
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 14:52:01 -0300

    helmet

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { ContactDirectInfo } from "../../Components/ContactDirectInfo/ContactDirectInfo";
 import { ContactForm } from "../../Components/ContactForm/ContactForm";
 import { Divider } from "../../Components/Divider/Divider";
+import { Helmet } from "react-helmet-async";
 
 import "./contact.css";
 
 export const Contact = () => {
 	return (
-		<div className='contactContainer'>
-			<Divider />
-			<ContactDirectInfo />
-			<Divider />
-			<Divider />
-			<ContactForm />
-		</div>
+		<>
+			<Helmet>
+				<title>1928 Showroom</title>
+				<meta
+					name='description'
+					content='Celebramos la belleza en todas sus formas. Todas las semanas nuevos ingresos.'
+				/>
+				<link rel='canonical' href='' />
+			</Helmet>
+			<div className='contactContainer'>
+				<Divider />
+				<ContactDirectInfo />
+				<Divider />
+				<Divider />
+				<ContactForm />
+			</div>
+		</>
 	);
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,20 +3,42 @@ import { ButtonMore } from "../../Components/ButtonMore/ButtonMore";
 import { Divider } from "../../Components/Divider/Divider";
 import { ItemShowcase } from "../../Components/ItemShowcase/ItemShowcase";
 import { SwiperHome } from "../../Components/SwiperHome/SwiperHome";
+import { Helmet } from "react-helmet-async";
+
 import "./home.css";
 
 export const Home = ({ value }) => {
 	return (
-		<div className='homeContainer'>
-			<SwiperHome />
-			<Divider />
-			<Divider />
-			<ItemShowcase value={value} />
-			<Divider />
-			<Divider />
-			<ButtonMore title={"Ver todos los productos"} path='productos' />
-			<Divider />
-			<Divider />
-		</div>
+		<>
+			<Helmet>
+				<title>1928 Showroom</title>
+				<meta
+					name='description'
+					content='Celebramos la belleza en todas sus formas. Todas las semanas nuevos ingresos.'
+				/>
+				<link rel='canonical' href='' />
+			</Helmet>
+			<div className='homeContainer'>
+				<h1 className='hiddenH1'>
+					1928 Showroom ropa mujer jeans remeras sweaters camperas
+					accesorios
+				</h1>
+				<SwiperHome />
+				<Divider />
+				<Divider />
+				<h2 className='homeTitle'>DESTACADOS</h2>
+				<Divider />
+
+				<ItemShowcase value={value} />
+				<Divider />
+				<Divider />
+				<ButtonMore
+					title={"Ver todos los productos"}
+					path='productos'
+				/>
+				<Divider />
+				<Divider />
+			</div>
+		</>
 	);
 };
