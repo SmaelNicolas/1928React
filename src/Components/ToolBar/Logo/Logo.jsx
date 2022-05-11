43fee015b2db97bdb9d36931835ca29cafa2c84f
Smael Nicolas <nico.smael@gmail.com>
2022-05-11 11:58:59 -0300

    logo layout

--- src/Components/ToolBar/Logo/Logo.jsx
+++ src/Components/ToolBar/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoMini from "../../../Assets/logoMini.png";
 
 import "./logo.css";
 
 export const Logo = () => {
 	return (
 		<Link className='logoName' to='/'>
-			1928Showroom
+			<p className='logoText'>Showroom</p>
+			<img className='logoImg' src={logoMini} alt='logo 1928' />
+			<p className='logoText'>Plus Size</p>
 		</Link>
 	);
 };
