32d3d0d7f24509fe9fb74a25ad3839818b8e84c6
Smael Nicolas <nico.smael@gmail.com>
2022-05-16 12:06:01 -0300

    change wtsp button

--- src/Components/WhatsAppButton/WhatsappButton.jsx
+++ src/Components/WhatsAppButton/WhatsappButton.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+// import imgWhatsapp from "../../Assets/wtsp.svg";
+import imgWhatsapp from "../../Assets/wtspgreen.svg";
 
 import "./whatsAppButton.css";
 import { openInNewTab } from "../../Helpers/openInNewTab";
 
 export const WhatsappButton = () => {
 	return (
-		<WhatsAppIcon
-			className='whatsAppButton'
+		<img
+			src={imgWhatsapp}
+			alt='wtsp redirect'
 			onClick={() =>
 				openInNewTab(
 					"https://api.whatsapp.com/send/?phone=542914182420"
 				)
 			}
+			className='whatsAppButton'
 		/>
+		// <imgWhatsapp
+		// 	className='whatsAppButton'
+		// 	onClick={() =>
+		// 		openInNewTab(
+		// 			"https://api.whatsapp.com/send/?phone=542914182420"
+		// 		)
+		// 	}
+		// />
 	);
 };
